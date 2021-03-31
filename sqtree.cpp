2ddef38c5b983f1b37df65df1b0e3c8d83734c45
Shashikiran Gonugunta <shashi090801@gmail.com>
2021-03-30 20:58:21 -0700

    stats

--- sqtree.cpp
+++ sqtree.cpp
@@ -43,7 +43,9 @@ SQtree & SQtree::operator=(const SQtree & rhs) {
  * SQtree constructor given tolerance for variance.
  */
 SQtree::SQtree(PNG & imIn, double tol) {
-  // Your code here.
+  pair<int,int> ul(0,0);
+  stats *s = new stats(imIn);
+  root = buildTree(*s, ul, imIn.width(), imIn.height(), tol);
 }
 
 /**
@@ -51,28 +53,119 @@ SQtree::SQtree(PNG & imIn, double tol) {
  */
 SQtree::Node * SQtree::buildTree(stats & s, pair<int,int> & ul,
 				 int w, int h, double tol) {
-  // Your code here.
+  if(s.getVar(ul,w,h) <= tol || (w == 1 && h == 1)){
+    return new Node(s, ul, w, h);
+  }    
+  Node * nod = new Node(s,ul,w,h);
+  Node * tempNW, *tempNE, *tempSW, *tempSE;
+  pair<int,int> TL, TR, BL, BR;
+  TL = ul;
+  TR.first = ul.first;
+  TR.second = h/2;
+  BL.first = w/2;
+  BL.second = ul.second;
+  BR.first = w/2;
+  BR.second = h/2;
+  tempNW = buildTree(s,TL,w/2,h/2,tol); 
+  tempNE = buildTree(s,TR,w/2,h/2,tol);
+  tempSW = buildTree(s,BL,w/2,h/2,tol);
+  tempSE = buildTree(s,BR,w/2,h/2,tol);
+  if(tempNW->var <= tol)
+    nod->NW = tempNW;
+  if(tempNW->var <= tol)
+    nod->NE = tempNE;
+  if(tempNW->var <= tol)
+    nod->SW = tempSW;
+  if(tempNW->var <= tol)
+    nod->SE = tempSE;  
+  return nod; 
 }
   
 /**
  * Render SQtree and return the resulting image.
  */
 PNG SQtree::render() {
-  // Your code here.
+  PNG *im = new PNG(root->width,root->height);
+  Imager(root,*im);
+  return *im;
 }
 
 /**
  * Delete allocated memory.
  */
 void SQtree::clear() {
-  // Your code here.
+  cleaner(root);  
 }
 
 void SQtree::copy(const SQtree & other) {
-  // Your code here.
+  copier(root,other.root);
 }
 
 int SQtree::size() {
-  // Your code here.
+  return len(0, root);
 }
 
+void SQtree::cleaner(Node * nod) {
+  if(nod == NULL)
+    return;
+  if(nod->NW != NULL)
+    cleaner(nod->NW);
+  if(nod->NE != NULL)
+    cleaner(nod->NE);
+  if(nod->SE != NULL)
+    cleaner(nod->SE);
+  if(nod->SW != NULL)
+    cleaner(nod->SW);
+  free(nod);
+}
+
+int SQtree::len(int count, Node * nod) {
+  if(nod == NULL)
+    return 0 + count;
+  if(nod->NW != NULL)
+    count = len(count, nod->NW);
+  if(nod->NE != NULL)
+    count = len(count, nod->NE);
+  if(nod->SE != NULL)
+    count = len(count, nod->SW);
+  if(nod->SW != NULL)
+    count = len(count, nod->SE);
+  return 1 + count;
+}
+
+SQtree::Node * SQtree::copier(Node* nod, Node* copiedNode) {
+  if(nod == NULL)
+    return NULL;
+  if(nod->NW != NULL)
+    copiedNode->NW = copier(nod->NW,copiedNode->NW);
+  if(nod->NE != NULL)
+    copiedNode->NE = copier(nod->NE,copiedNode->NE);
+  if(nod->SE != NULL)
+    copiedNode->SW = copier(nod->SW,copiedNode->SW);
+  if(nod->SW != NULL)
+    copiedNode->SE = copier(nod->SE,copiedNode->SE);
+  return new Node(nod->upLeft,nod->width,nod->height,nod->avg,nod->var);
+}
+
+void SQtree::Imager(Node* nod, PNG im) {
+  if(nod == NULL)
+    return;
+  for(int x = nod->upLeft.first; x < nod->width; x++) {
+    for(int y = nod->upLeft.second; y < nod->height; y++) {
+      RGBAPixel *pix = im.getPixel(x,y);
+      *pix = nod->avg;
+    }
+  }
+  if(nod->NW != NULL){
+    Imager(nod->NW, im);
+  }
+  if(nod->NE != NULL){
+    Imager(nod->NE, im);
+  }
+  if(nod->SW != NULL){
+    Imager(nod->SW, im);
+  }
+  if(nod->SW != NULL){
+    Imager(nod->SE, im);
+  }
+}
--- stats.cpp
+++ stats.cpp
@@ -4,28 +4,109 @@
 #include "stats.h"
 
 stats::stats(PNG & im){
-
-/* Your code here!! */
-
+    for(unsigned x = 0; x < im.width(); x++){
+        vector<long> sR, sB, sG, sqR, sqB, sqG;
+        for(unsigned y = 0; y < im.height(); y++){        
+            long sumr = 0, sumb = 0, sumg = 0, sumsr = 0, sumsb = 0, sumsg = 0;  
+            for(unsigned i = 0; i <= x; i++) {
+                for(unsigned j = 0; j <= y; j++) {        
+                    RGBAPixel *pix = im.getPixel(i,j);
+                    sumr += pix->r;
+                    sumb += pix->b;
+                    sumg += pix->g;
+                    sumsr += pow(pix->r,2);
+                    sumsb += pow(pix->b,2);
+                    sumsg += pow(pix->g,2);
+                }
+            }
+            sR.push_back(sumr);
+            sB.push_back(sumb);
+            sG.push_back(sumg);
+            sqR.push_back(sumsr);
+            sqB.push_back(sumsb);
+            sqG.push_back(sumsg);
+        }
+        sumRed.push_back(sR);
+        sumBlue.push_back(sB);
+        sumGreen.push_back(sG);
+        sumsqRed.push_back(sqR);
+        sumsqBlue.push_back(sqB);
+        sumsqGreen.push_back(sqG);
+    }
 }
 
 
 long stats::getSum(char channel, pair<int,int> ul, int w, int h){
-/* Your code here!! */
+    long sum = 0;
+    switch(channel){
+        case 'r':
+            for(int x = ul.first; x < w; x++){
+                for(int y = ul.second; y < h; y++){
+                    sum += sumRed[x][y];
+                } 
+            }
+            break;
+        case 'b':
+            for(int x = ul.first; x < w; x++){
+                for(int y = ul.second; y < h; y++){
+                    sum += sumBlue[x][y];
+                }
+            }
+            break;
+        case 'g':
+            for(int x = ul.first; x < w; x++){
+                for(int y = ul.second; y < h; y++){
+                    sum += sumGreen[x][y];
+                }
+            }
+            break;
+        default: break;
+    }
+    return sum;
 }
 
 long stats::getSumSq(char channel, pair<int,int> ul, int w, int h){
-/* Your code here!! */
+    long sum = 0;
+    switch(channel){
+        case 'r':
+            for(int x = ul.first; x < w; x++){
+                for(int y = ul.second; y < h; y++){
+                    sum += sumsqRed[x][y];
+                }
+            }
+            break;
+        case 'b':
+            for(int x = ul.first; x < w; x++){
+                for(int y = ul.second; y < h; y++){
+                    sum += sumsqBlue[x][y];
+                }
+            }
+            break;
+        case 'g':
+            for(int x = ul.first; x < w; x++){
+                for(int y = ul.second; y < h; y++){
+                    sum += sumsqGreen[x][y];
+                }
+            }
+            break;
+        default: break;
+    }
+    return sum;
 }
 
 // given a rectangle, compute its sum of squared deviations from mean, over all color channels.
 // see written specification for a description of this function.
 double stats::getVar(pair<int,int> ul, int w, int h){
-/* Your code here!! */
+    RGBAPixel mean = getAvg(ul,w,h);
+    double redVar = getSumSq('r', ul, w, h) - pow(mean.r,2);
+    double greenVar = getSumSq('g', ul, w, h) - pow(mean.g,2);
+    double blueVar = getSumSq('b', ul, w, h) - pow(mean.b,2);
 
+    return (redVar+greenVar+blueVar);
+    
 }
 		
 RGBAPixel stats::getAvg(pair<int,int> ul, int w, int h){
-/* Your code here!! */
-
+    RGBAPixel *pix = new RGBAPixel(sumRed[w-1][h-1]/(w*h),sumGreen[w-1][h-1]/(w*h),sumBlue[w-1][h-1]/(w*h));
+    return *pix;
 }
